37cdaac875e8d8e9f9c1ea8abd749223ffccee7c
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2022-11-01 17:35:36 +0200

    Work on front and back end

---
+++ server.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Printf("Starting server at port 8080\n")
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+	
+}
