439d52ef83db0ce66bf8e6da0442026af6bcdcdd
Falusvampen <102164648+Falusvampen@users.noreply.github.com>
2022-11-14 18:12:10 +0200

    moved to scripts/asciiart

---
+++ scripts/asciiart/dnadiff.go
@@ -0,0 +1,6 @@
+package asciiart
+
+func DnaDiff() string {
+	ayyy := "╭━━━━-╮ &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<br />╰┃ ┣▇━▇ &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<br />┃ ┃  ╰━▅╮ &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<br /> ╰┳╯ ╰━━┳╯ DNA DIFF &nbsp; &nbsp; &nbsp;<br />  ╰╮ ┳━━╯ GG EZ &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<br /> ▕▔▋ ╰╮╭━╮NICE TUTORIAL<br />╱▔╲▋╰━┻┻╮╲╱▔▔▔╲ &nbsp; &nbsp; &nbsp;<br />▏  ▔▔▔▔▔▔▔  O O┃ &nbsp;&nbsp;<br />╲╱▔╲▂▂▂▂╱▔╲▂▂▂╱ &nbsp;&nbsp;<br /> ▏╳▕▇▇▕ ▏╳▕▇▇▕ &nbsp;&nbsp;<br /> ╲▂╱╲▂╱ ╲▂╱╲▂╱ &nbsp; &nbsp; &nbsp; &nbsp;"
+	return ayyy
+}
